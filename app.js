6bb7377bb45b887bee283a33f21db5adf14520f7
Sanket Ranpariya <patelsanket7261@gmail.com>
2024-08-01 12:28:47 +0530

    fixed the bug during delete process

--- app.js
+++ app.js
@@ -1,141 +1,139 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const User = require('./models/user');
+const express = require("express");
+const mongoose = require("mongoose");
+const User = require("./models/user");
 
 // express app
 const app = express();
 
 // connect to mongodb & listen for requests
-const dbURI = "paste here your mongodb uri that can be get form connect button";
+const dbURI = "mongodb://0.0.0.0:27017/";
 
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true }) //this return promise
-  .then((result) =>{ console.log("Database-connected"); app.listen(8080)})
-  //after db connected than it will listen to port3000
-  .catch(err => console.log(err)); //else errors will be shown
+async function connectToDatabase() {
+  try {
+    await mongoose.connect(dbURI);
+    console.log("Database connected");
+    // After database is connected, listen to port 8080
+    app.listen(8080, () => {
+      console.log("server is running on port 8080");
+    });
+  } catch (err) {
+    console.log(err); // Else errors will be shown
+  }
+}
 
+connectToDatabase();
 
 // register view engine
-app.set('view engine', 'ejs');
- 
+app.set("view engine", "ejs");
+
 // middleware & static files
-app.use(express.static('public')); //this will helps to use style.css file
+app.use(express.static("public")); //this will helps to use style.css file
 app.use(express.urlencoded({ extended: true })); //this will helps to get submitted data of form in req.body obj
 
-
-
 // home routes
-app.get('/', (req, res) => {
-  res.redirect('/users'); //this will redirect page to /users
+app.get("/", (req, res) => {
+  res.redirect("/users"); //this will redirect page to /users
 });
 
-//users i.e index route
-app.get('/users',(req,res)=>{
-  console.log("req made on"+req.url);
-   User.find().sort({createdAt:-1})//it will find all data and show it in descending order
-    .then(result => { 
-      res.render('index', { users: result ,title: 'Home' }); //it will then render index page along with users
-    })
-    .catch(err => {
-      console.log(err);
-    });
-})
+// users i.e index route
+app.get("/users", async (req, res) => {
+  console.log("req made on " + req.url);
+  try {
+    const result = await User.find().sort({ createdAt: -1 }); // It will find all data and show it in descending order
+    res.render("index", { users: result, title: "Home" }); // It will then render the index page along with users
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("An error occurred"); // Optionally, you can send an error response to the client
+  }
+});
 
-//about route
-app.get('/about',(req,res)=>{
-  console.log("req made on"+req.url);
-  res.render('about',{title:'About'});
-})
+// about route
+app.get("/about", (req, res) => {
+  console.log("req made on " + req.url);
+  res.render("about", { title: "About" });
+});
 
-//route for user create
-app.get('/user/create',(req,res)=>{
-  console.log("GET req made on"+req.url);
-  res.render('adduser',{title:'Add-User'});
-})
+// route for user create
+app.get("/user/create", (req, res) => {
+  console.log("GET req made on " + req.url);
+  res.render("adduser", { title: "Add-User" });
+});
 
-//route for users/withvar
-app.get('/users/:id', (req, res) => {
+// route for users/withvar
+app.get("/users/:id", async (req, res) => {
   const id = req.params.id;
-  User.findById(id)
-    .then(result => {
-      res.render('details', { user: result, action:'edit',title: 'User Details' });
-    })
-    .catch(err => {
-      console.log(err);
+  try {
+    const result = await User.findById(id);
+    res.render("details", {
+      user: result,
+      action: "edit",
+      title: "User Details",
     });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("An error occurred"); // Optionally, you can send an error response to the client
+  }
 });
 
-//route for edit/name/action variable that will display current value to input field
-app.get('/edit/:name/:action',(req,res)=>{
+// route for edit/name/action variable that will display current value to input field
+app.get("/edit/:name/:action", async (req, res) => {
   const name = req.params.name;
-  console.log("req made on"+req.url);
-  User.findOne({name:name})
-    .then(result => {
-      res.render('edit', { user: result ,title: 'Edit-User' });
-    })
-    .catch(err => {
-      console.log(err);
-    });
-})
+  console.log("req made on " + req.url);
+  try {
+    const result = await User.findOne({ name: name });
+    res.render("edit", { user: result, title: "Edit-User" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("An error occurred"); // Optionally, you can send an error response to the client
+  }
+});
 
-//submitting data routes
-  app.post('/user/create',(req,res)=>{
-  console.log("POST req made on"+req.url);
+// submitting data routes
+app.post("/user/create", async (req, res) => {
+  console.log("POST req made on " + req.url);
   console.log("Form submitted to server");
 
+  try {
+    const user = new User(req.body); //passing object of form data directly to collection
+    await user.save(); //then saving this to database
+    res.redirect("/users"); //if success, redirect to home page
+  } catch (err) {
+    console.log(err); //if data not saved, error showed
+    res.status(500).send("An error occurred");
+  }
+});
 
-  /*Note: when you are passing form obj directly to collection using model you
-          have to give same name in form of that data that is to be passed in database 
-          and that name is declared inside schema*/
-  const user = new User(req.body); //passing object of form data directly to collection
-  user.save() //then saving this to database and this return promise
-    .then(result => {
-      res.redirect('/users');//is success save this will redirect to home page
-    })
-    .catch(err => { //if data not saved error showed
-      console.log(err);
-    });
+// route for updating users data
+app.post("/edit/:id", async (req, res) => {
+  console.log("POST req made on " + req.url);
+  try {
+    await User.updateOne({ _id: req.params.id }, req.body); //updating the user whose id is get from url
+    res.redirect("/users"); //if success, redirect to home page
+    console.log("User's profile updated");
+  } catch (err) {
+    console.log(err); //if data not saved, error showed
+    res.status(500).send("An error occurred");
+  }
+});
 
-})
-
-//route for updating users data
-app.post('/edit/:id',(req,res)=>{
-  console.log("POST req made on"+req.url);
-  User.updateOne({_id:req.params.id},req.body) //then updating that user whose id is get from url 
-                                               //first passing id which user is to be updated than passing update info
-    .then(result => {
-      res.redirect('/users');//is success save this will redirect to home page
-      console.log("Users profile Updated");
-    })
-    .catch(err => { //if data not saved error showed
-      console.log(err);
-    });
-
-})
-
-
-//routes for deleting users by getting users name from url then finding that  users then doing delete
-app.post('/users/:name',(req,res)=>{ //form action of details.ejs pass name of user that later is assume as name
+// routes for deleting users by getting user's name from url then finding that user and deleting
+app.post("/users/:name/delete", async (req, res) => {
+  // Updated to have a unique URL path for deletion
   const name = req.params.name;
   console.log(name);
-  User.deleteOne({name:name})
-  .then(result => {
-    res.redirect('/users');
-  })
-  .catch(err => {
+  try {
+    await User.deleteOne({ name: name });
+    res.redirect("/users");
+  } catch (err) {
     console.log(err);
-  });
-})
+    res.status(500).send("An error occurred");
+  }
+});
 
-//404 errors routes
-//this will auto run incase no routes
-//Note: must put this route at last route list
-app.use((req,res)=>{
-  console.log("req made on"+req.url);
-  res.render('404',{title:'NotFound'});
-})
-
-
-
-
-
-
+// 404 errors route
+// This will auto run in case no routes match
+// Note: must put this route at last in the route list
+app.use((req, res) => {
+  console.log("req made on " + req.url);
+  res.render("404", { title: "NotFound" });
+});
